0486233952a81cb06d44968317f3d03b7ed44884
ashish mittal <amittal071@gmail.com>
2023-01-08 18:12:42 +0530

    added custom deploy action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,10 +1,8 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
+// const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-  core.notice('Hello From My Custom JavaScript Action!');
-  /*
   // 1) Get some input values
   const bucket = core.getInput('bucket', { required: true });
   const bucketRegion = core.getInput('bucket-region', { required: true });
@@ -14,8 +12,9 @@ function run() {
   const s3Uri = `s3://${bucket}`;
   exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-  const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
-  core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
-*/
+  //const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+  //core.setOutput('website-url', websiteUrl); // echo "website-url=..." >> $GITHUB_OUTPUT
+
+  core.notice('Hello From My Custom JavaScript Action!');
 }
 run();
